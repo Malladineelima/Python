2151e97be5bfad140548b430918533453dcc82e4
Malladineelima <124678063+Malladineelima@users.noreply.github.com>
2023-03-09 18:31:59 +0530

    Update DAY 1.py

--- DAY 1.py
+++ DAY 1.py
@@ -7,38 +7,21 @@ print(num)
 print(amt)
 print(code)
 print(msg)
-#o/p=
-5
-122.122
-n
-hi
 
 #program 2
 a=int(input())
 A=int(input())
 print("a=",a)
 print("A=",A)
-o/p=
-15
-30
-a= 15
-A= 30
 
 #program 3
 a=int(input())
 a=int(input())
 print("a=",a)
-o/p=
-27
-103
-a= 103
 
 #program 4
 print('neelu'+'27')
 print(2+7)
-o/p
-neelu27
-9
 
 #program 5
 a=10
@@ -50,13 +33,6 @@ a,b=b,a
 print("after swap")
 print('a=',a)
 print('b=',b)
-o/p=
-before swap
-a= 10
-b= 20
-after swap
-a= 20
-b= 10
 
 #program 6
 a=int(input("enter a number="))
@@ -65,20 +41,12 @@ c=int(input("enter a number="))
 s=a+b+c/2
 area=(s*(s-a)*(s-b)*(s-c)**0.5)
 print(area)
-o/p=
-enter a number=3
-enter a number=6
-enter a number=9
-2255.228690246845
 
 #program 7
 r=int(input("enter a number"))
 pi=3.141
 circumferrance=2*pi*r
 print(circumferrance)
-o/p=
-enter a number15
-94.23
 
 
 #progrsm 8
@@ -87,9 +55,6 @@ if a%2==0:
       print('a is even',a)
 else:
     print('a is odd',a)
-o/p=
-enter a number7
-a is odd 7
 
 #program 9
 a=int(input("enter a number"))
@@ -98,9 +63,6 @@ if a>b:
       print("a is greater number")
 else:
       print("b is greater number")
-o/p=enter a number15
-enter a number25
-b is greater number
 
 #program 10
 
@@ -111,8 +73,6 @@ elif n<0:
       print(n,"is negative number")
 else:
       print(n, "is zero")
-o/p=enter a number-10
--10 is negative number
 
 #program 11
 
@@ -123,11 +83,6 @@ if n>=0 and n<20:
       print(n,"is between 10  to 20")
 if n>=20 and n<30:
       print(n,"is between 20 tp 30")
-o/p=
-enter a number below 30=10
-10 is between 10  to 20
-enter a number below 30=15
-15 is between 10  to 20
 
 #program 12
 
@@ -143,12 +98,7 @@ elif b>c:
 elif c>a:
       if c>b:
             print("c is max")
-o/p=
-enter a=5
-enter b=30
-enter c=20
-b is max'''
-      
+    
 
 
 
